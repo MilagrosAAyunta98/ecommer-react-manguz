10d7ab19c0aa86325678c804b22aa399e002df03
Milagros  Ayunta <99821270+MilagrosAAyunta98@users.noreply.github.com>
2022-05-18 19:51:19 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import NavBar from './Components/NavBar/NavBar'
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Manguz Ecommers</h1>
     </div>
   );
 }
 
 export default App;
+
